b8f96d5e50bd47e9f47d7172dac6b2aada6fbf2a
mikudd3 <96935176+mikudd3@users.noreply.github.com>
2023-04-18 09:28:16 +0800

    Add files via upload

---
+++ mythreadtest/src/com/itheima/test2/Test.java
@@ -0,0 +1,21 @@
+package com.itheima.test2;
+
+public class Test {
+    public static void main(String[] args) {
+        /*
+            有100份礼品,两人同时发送，当剩下的礼品小于10份的时候则不再送出，
+            利用多线程模拟该过程并将线程的名字和礼物的剩余数量打印出来.
+        */
+
+        //创建参数对象
+        MyRunable mr = new MyRunable();
+
+        //创建线程对象
+        Thread t1 = new Thread(mr,"窗口1");
+        Thread t2 = new Thread(mr,"窗口2");
+
+        //启动线程
+        t1.start();
+        t2.start();
+    }
+}
