3ea15fceb028d0afbefab30f5c095776138e8321
mikudd3 <96935176+mikudd3@users.noreply.github.com>
2023-04-18 09:20:36 +0800

    Add files via upload

---
+++ myfunction/a01myfunction/StringOperation.java
@@ -0,0 +1,7 @@
+package com.itheima.a01myfunction;
+
+public class StringOperation {
+    public boolean stringJudge(String s){
+        return s.startsWith("张") && s.length() == 3;
+    }
+}
---
+++ myfunction/a02game/App.java
@@ -0,0 +1,7 @@
+package com.itheima.a02game;
+
+public class App {
+    public static void main(String[] args) {
+        new LoginJFrame();
+    }
+}
